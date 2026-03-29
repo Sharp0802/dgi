d1fa20ec1ef1e230db8c91a237849555505e011d
sharp0802 <sharp0802.business@gmail.com>
2026-03-29 19:11:12 +0900

    :recycle: Simplify thread ids by mapping into linear map

--- crates/log/src/event.rs
+++ crates/log/src/event.rs
@@ -1,7 +1,10 @@
 use chrono::{DateTime, Utc};
+use dashmap::DashMap;
 use serde::Serialize;
 use serde_json::Value;
 use serde_repr::Serialize_repr;
+use std::sync::LazyLock;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 #[derive(Serialize_repr, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
 #[repr(u8)]
@@ -41,12 +44,22 @@ pub struct Event {
     pub fields: Vec<Field>,
 }
 
+static TID: AtomicUsize = AtomicUsize::new(0);
+static TID_MAP: LazyLock<DashMap<usize, usize>> = LazyLock::new(|| DashMap::new());
+
+fn get_tid() -> usize {
+    *TID_MAP
+        .entry(thread_id::get())
+        .or_insert_with(|| TID.fetch_add(1, Ordering::AcqRel))
+        .value()
+}
+
 impl Event {
     pub fn new(level: Level, module: &'static str, message: String, fields: Vec<Field>) -> Self {
         Self {
             timestamp: Utc::now(),
             level,
-            thread_id: thread_id::get(),
+            thread_id: get_tid(),
             module,
             message,
             fields,

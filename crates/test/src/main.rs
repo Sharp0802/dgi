92a8c26737fab651118816262bb2094491f7c49f
sharp0802 <sharp0802.business@gmail.com>
2026-03-31 17:42:02 +0900

    :recycle: Update versions

--- crates/test/src/main.rs
+++ crates/test/src/main.rs
@@ -1,14 +1,5 @@
-use dgi::log::impls::Console;
-use dgi::log::prelude::*;
 use dgi::shell::app::App;
 
 fn main() {
-    let logger = builder()
-        .writer(Console::new().max_verbosity(Verbosity::Debug))
-        .run()
-        .unwrap();
-
     App::new().run();
-
-    logger.stop();
 }
